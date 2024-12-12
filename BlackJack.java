359cd90e5384cecb31151c1d71f9d2e912bef0e9
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2024-12-12 08:27:34 -0800

    added base game logic, dealer and player hand
    now to build UI

--- BlackJack.java
+++ BlackJack.java
@@ -17,10 +17,35 @@ public class BlackJack {
         public String toString() {
             return value + "-" + type;
         }
+
+        public int getValue() {
+            if ("AJQK".contains(value)) {// A J Q K
+                if (value == "A") {
+                    return 11;
+                }
+                return 10;
+            }
+            return Integer.parseInt(value);// return 2 - 10
+        }
+
+        public boolean isAce() {
+            return value == "A";
+        }
     }
 
     ArrayList<Card> deck;
     Random random = new Random(); // shuffle thee deck
+
+    // dealer
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum;
+    int dealerAceCount;// if dealer passes 21 when he has ACE make it count as 1
+
+    // player
+    ArrayList<Card> playerHand;
+    int playerSum;
+    int playerAceCount;
 
     BlackJack() {
         startGame();
@@ -33,6 +58,42 @@ public class BlackJack {
         // lets shuffle this ****
         shuffleDeck();
 
+        // dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount = 0;
+
+        hiddenCard = deck.remove(deck.size() - 1); // remove card at last index
+        dealerSum += hiddenCard.getValue();
+        dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+        Card card = deck.remove(deck.size() - 1);
+        dealerSum += card.getValue();
+        dealerAceCount += card.isAce() ? 1 : 0;
+        dealerHand.add(card);
+
+        System.out.println("Dealer:");
+        System.out.println(hiddenCard);
+        System.out.println(dealerHand);
+        System.out.println(dealerSum);
+        System.out.println(dealerAceCount);
+
+        // player
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
+
+        for (int i = 0; i < 2; i++) {
+            card = deck.remove(deck.size() - 1);
+            playerSum += card.getValue();
+            playerAceCount += card.isAce() ? 1 : 0;
+            playerHand.add(card);
+        }
+
+        System.out.println("Player:");
+        System.out.println(playerHand);
+        System.out.println(playerSum);
+        System.out.println(playerAceCount);
     }
 
     public void buildDeck() {
